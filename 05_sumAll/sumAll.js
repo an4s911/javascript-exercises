df839d1cc63516115163d1258317a41073c6d909
Anas Basheer <nasbas23@gmail.com>
2022-01-16 14:57:08 +0300

    Completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let sum = 0;
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 
+    if (typeof num1 !== typeof num2) {
+        return "ERROR";
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        if (i < 0) return "ERROR";
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
