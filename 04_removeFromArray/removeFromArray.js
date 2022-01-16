a690f7ffcb690364513f135235b92c1c1a571929
Anas Basheer <nasbas23@gmail.com>
2022-01-16 14:45:53 +0300

    Completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    for (const item of arguments) {
+        if (item === array) continue;
 
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            if (element === item) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
