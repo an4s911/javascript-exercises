3ca545238574471ccc5f5c9cc12ab0d017a58aa0
Anas Basheer <nasbas23@gmail.com>
2022-01-19 15:38:49 +0300

    Completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const reverseString = (string) => {
+    let reversed = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i];
+    }
+    return reversed;
+}
 
+const palindromes = function (string) {
+    let cleanString = "";
+    let lowercaseAlphaRegEx = /[a-z]/i;
+    for (let ch of string.toLowerCase()) {
+        cleanString += lowercaseAlphaRegEx.test(ch) ? ch : "";
+    }
+
+    return cleanString === reverseString(cleanString);
 };
 
 // Do not edit below this line
