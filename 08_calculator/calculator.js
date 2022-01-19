e450086937ba53642bc1115fbb7070dd8d7a51f5
Anas Basheer <nasbas23@gmail.com>
2022-01-19 11:46:28 +0300

    Completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return add(a, -b);
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  let arraySum = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    arraySum += numArray[i];
+  }
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  let product = 1;
+  for (let i = 0; i < numArray.length; i++) {
+    product *= numArray[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 1;
+  for (let i = num; i >= 1; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
