452ccba9ef3e5cb135ac32a50d5c80de3b41b648
Anas Basheer <nasbas23@gmail.com>
2022-01-19 16:04:58 +0300

    Completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    return array.slice(1).reduce((obj, item) => {
+        const prevAge = getAge(obj);
+        const currAge = getAge(item);
+        return currAge > prevAge ? item : obj;
+    }, array[0]);
 };
+
+function getAge(person) {
+    if ("yearOfDeath" in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    const currYear = (new Date()).getFullYear();
+    return currYear - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
